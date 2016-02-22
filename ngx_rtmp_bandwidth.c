f38e3e0a88266d3196ff3f869737b5b691d00e9f
myself659 <myself659@gmail.com>
2016-02-22 12:22:33 +0800

    bandwidth ok

--- ngx_rtmp_bandwidth.c
+++ ngx_rtmp_bandwidth.c
@@ -12,15 +12,16 @@
 void
 ngx_rtmp_update_bandwidth(ngx_rtmp_bandwidth_t *bw, uint32_t bytes)
 {
+	/* 10s计算一次 */
     if (ngx_cached_time->sec > bw->intl_end) {
         bw->bandwidth = ngx_cached_time->sec >
             bw->intl_end + NGX_RTMP_BANDWIDTH_INTERVAL
             ? 0
             : bw->intl_bytes / NGX_RTMP_BANDWIDTH_INTERVAL;
-        bw->intl_bytes = 0;
-        bw->intl_end = ngx_cached_time->sec + NGX_RTMP_BANDWIDTH_INTERVAL;
+        bw->intl_bytes = 0; /* 间隔内的统计清0 */
+        bw->intl_end = ngx_cached_time->sec + NGX_RTMP_BANDWIDTH_INTERVAL;  /* 一个统计周期结束时间 */
     }
 
-    bw->bytes += bytes;
+    bw->bytes += bytes; /* 总的字节计算  */
     bw->intl_bytes += bytes;
 }
